b1dc940737ef61395a2c2fefed8d30902fd2e6db
Abdelrahman gomaa <100715853+Abdelrahman-G@users.noreply.github.com>
2023-10-27 14:39:58 +0200

    add 2 functions
    mkdir and rmdir (needs fixes)

---  Command_Line_Interpreter/src/Terminal.java
+++  Command_Line_Interpreter/src/Terminal.java
@@ -1,8 +1,5 @@
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Terminal {
     Parser parser;
@@ -83,7 +80,7 @@ public void cd(String[] args){
 public List<String> ls() {
     List<String> fileList = new ArrayList<>();
     File directoryPath = new File(pwd());
-    String contents[] = directoryPath.list();
+    String[] contents = directoryPath.list();
     Arrays.sort(contents);
     for(int i=0; i<contents.length; i++) {
         fileList.add(contents[i]);
@@ -99,7 +96,7 @@ public List<String> ls() {
 public List<String> ls_r() {
     List<String> fileList = new ArrayList<>();
     File directoryPath = new File(pwd());
-    String contents[] = directoryPath.list();
+    String[] contents = directoryPath.list();
     if (contents != null) {
         Arrays.sort(contents, Collections.reverseOrder()); 
         fileList.addAll(Arrays.asList(contents));
@@ -112,30 +109,77 @@ public List<String> ls_r() {
 public void chooseCommandAction(){}
 
 
-
-
-
-public static void main(String[] args){
-    Terminal terminal = new Terminal();
-    terminal.parser = new Parser();
-    while(true){
-        System.out.print("user@user:~$ ");
-        String input = System.console().readLine();
-        terminal.parser.parse(input);
-        terminal.chooseCommandAction();
+    public boolean FullPath(String str) {
+        return str.contains("C:\\Users");
     }
-}
+    /*
+    ... for creating any number of paths
+   */
+    public void mkdir(String... paths) {
 
-// public static void main(String[] args) {
-//     Terminal terminal = new Terminal();
-//     String[] cdArgs = {"E:\\FCAI\\Third_Year\\Third_Year(1)\\OS\\test"};
-//     terminal.cd(cdArgs);
-//     List<String> fileList = terminal.ls();
-//     for (String file : fileList) {
-//         System.out.println(file);
-//     }
-// }
+        for (String path : paths) {
+            /*mkdir function returns true if the directory is created, false otherwise*/
+            boolean flag;
+            if (!FullPath(path)) {
+                /*
+                 * if the given string is directory name not a full path
+                 * a new directory is created in the current directory.
+                 */
+                String new_directory = pwd() + path;
+                flag = new File(new_directory).mkdir();
+            }
+            else {
+                //In case the given string is the full path, create the directory
+                flag = new File(path).mkdir();
+            }
+
+            if (!flag) {
+                System.out.println("Directory is not created");
+            }
+        }
+    }
 
 
+    /*remove empty files in a directory*/
+    public void RemoveEmptyFiles(File current_directory){
+        /*exception is thrown if the directory is empty*/
+        for(File file : Objects.requireNonNull(current_directory.listFiles())){
+            if (file.isDirectory() && Objects.requireNonNull(file.list()).length==0) {
+                boolean deleted = file.delete();
+                if (!deleted)
+                    System.out.println("this directory is not deleted");
+            }
+        }
+    }
+
+    public void rmdir(String path) {
+        /*
+         * if the given argument = "*"remove
+         * all the empty directories in the current directory
+         */
+        File current_directory;
+        if (Objects.equals(path, "*")) {
+            current_directory = new File(pwd());
+            RemoveEmptyFiles(current_directory);
+        } else {
+            // this part still needs to be updated
+            // the code removes file if name of full path is given but not in case relative (short) path is given
+            if (FullPath(path)){
+                current_directory = new File(path);
+                current_directory.delete();
+                return;
+            }
+            String full_path = new File(path).getAbsolutePath();
+            current_directory = new File(full_path);
+            boolean deleted = current_directory.delete();
+            if (!deleted)
+                System.out.println("this directory is not deleted");
+        }
+
+    }
+
+    public void touch(String path) {
+        
+    }
 
 }
